7621089289127f49a984e63d68959d6900f55020
Muhammad Danish <14832486+idanish@users.noreply.github.com>
2024-10-21 02:54:37 +0000

    Javascript Projects

--- Project 3 - Loop - Data Shown in Table using Array/script.js
+++ Project 3 - Loop - Data Shown in Table using Array/script.js
@@ -1,20 +1,38 @@
 
+let ausername = ['user-danish1', 'user-danish2', 'user-danish3']
+let aname = ['Danish1', 'Danish2', 'Danish3']
+let aemail = ['danish1@email.com', 'danish2@email.com', 'danish3@email.com']
 
 
-function login(){
-let user = "user";
-let pass = "123";
-let username = document.getElementById('username').value;
-let password = document.getElementById('password').value;
+let busername = document.getElementById('txt-username').value;
+let bname = document.getElementById('txt-name').value;
+let bemail = document.getElementById('txt-email').value;
 
-if(!username || !password){
-// alert('Enter both username or Password')
+
+
+function btnSubmit() {
+    document.getElementById('a').innerHTML = busername;
+    document.getElementById('b').innerHTML = bname;
+    document.getElementById('c').innerHTML = bemail;
 }
 
-    if (user === username && pass === password) {
-          alert('success');
-    } else {
-        // alert('Enter correct username or password');
-    }
+
+
+// function btnSubmit(){
+
+//     ausername.push(busername)
+//     aname.push(bname)
+//     aemail.push(bemail)
+// }
+
+
+for (let i = 0; i < ausername.length; i++) {
+
+    document.getElementById('srNo').innerHTML += 1 + i + "</br>";
+    document.getElementById('username').innerHTML += ausername[i] + "</br>";
+    document.getElementById('name').innerHTML += aname[i] + "</br>";
+    document.getElementById('email').innerHTML += aemail[i] + "</br>";
 
 }
+
+
